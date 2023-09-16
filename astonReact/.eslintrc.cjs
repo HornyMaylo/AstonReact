96d22bd1d9f6157856cea056d17e762d85e1f3ea
HornyMaylo <belgkurmen@mail.ru>
2023-09-16 03:25:02 +0300

    ready ESLint

--- astonReact/.eslintrc.cjs
+++ astonReact/.eslintrc.cjs
@@ -6,16 +6,39 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-    'prettier'
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'import/order': [
+      'off',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        'newlines-between': 'always-and-inside-groups',
+      },
     ],
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+    'import/prefer-default-export': 'off',
+    'react/jsx-no-useless-fragment': 'error',
+    'no-unused-expressions': [
+      'error',
+      { allowShortCircuit: true, allowTernary: true },
+    ],
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
   },
-}
+};
--- astonReact/src/main.jsx
+++ astonReact/src/main.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import './index.css';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root';
 
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>,
 );
